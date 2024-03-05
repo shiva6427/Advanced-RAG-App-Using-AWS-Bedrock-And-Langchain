1214f41a3c9da5da213ebc6a91746795ce3ecbf5
Shiva Vangari <49754508+shiva6427@users.noreply.github.com>
2024-03-04 20:56:47 -0500

    Added files

---
+++ app.py
@@ -0,0 +1,179 @@
+import json
+import os
+import sys
+import boto3
+import streamlit as st
+
+## Titan Embeddings Model To generate Embedding
+
+from langchain_community.embeddings import BedrockEmbeddings
+from langchain.llms.bedrock import Bedrock
+
+## Data Ingestion
+
+import numpy as np
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+
+# Vector Embedding And Vector Store
+
+from langchain.vectorstores import FAISS
+
+## LLm Models
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+## Bedrock Clients
+bedrock=boto3.client(service_name="bedrock-runtime")
+bedrock_embeddings=BedrockEmbeddings(model_id="amazon.titan-embed-text-v1",client=bedrock)
+
+
+## Data ingestion
+def data_ingestion():
+    loader=PyPDFDirectoryLoader("data")
+    documents=loader.load()
+
+    # Character split 
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=10000,
+                                                 chunk_overlap=1000)
+    
+    docs=text_splitter.split_documents(documents)
+    return docs
+
+## Vector Embedding and vector store
+
+def get_vector_store(docs):
+    vectorstore_faiss=FAISS.from_documents(
+        docs,
+        bedrock_embeddings
+    )
+    vectorstore_faiss.save_local("faiss_index")
+
+def get_claude_llm():
+    ##create the Anthropic Model
+    llm=Bedrock(model_id="anthropic.claude-instant-v1",client=bedrock,
+                model_kwargs={'maxTokens':512})
+    
+    return llm
+
+def get_llama2_llm():
+    ##create the llama Model
+    llm=Bedrock(model_id="meta.llama2-70b-chat-v1",client=bedrock,
+                model_kwargs={'max_gen_len':512})
+    
+    return llm
+
+def get_titan_llm():
+    ##create the titan Model
+    llm=Bedrock(model_id="amazon.titan-text-lite-v1",client=bedrock,
+                model_kwargs={'max_gen_len':512})
+    
+    return llm
+
+def get_cohere_llm():
+    ##create the cohere Model
+    llm=Bedrock(model_id="cohere.command-light-text-v14",client=bedrock,
+                model_kwargs={'max_gen_len':512})
+    
+    return llm
+
+def get_jurassic_llm():
+    ##create the jurassic Model
+    llm=Bedrock(model_id="ai21.j2-mid-v1",client=bedrock,
+                model_kwargs={'max_gen_len':512})
+    
+    return llm
+
+prompt_template = """
+
+Human: Kindly utilize the provided context to deliver a concise answer to the question, aiming for a summary within a maximum limit of 100 words with detailed explanations. If uncertain, please acknowledge that you do not know instead of fabricating a response.
+
+<context>
+{context}
+</context
+
+Question: {question}
+
+Assistant:"""
+
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+def get_response_llm(llm,vectorstore_faiss,query):
+    qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore_faiss.as_retriever(
+        search_type="similarity", search_kwargs={"k": 3}
+    ),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": PROMPT}
+)
+    answer=qa({"query":query})
+    return answer['result']
+
+
+def main():
+    st.set_page_config("Chat PDF")
+    
+    st.header("📚🤖💬 Welcome to GeniePDF Chat")
+    st.subheader("An Interactive PDF Companion powered by AWS Bedrock 🚀")
+    user_question = st.text_input("Ask any Question from the PDF File")
+
+    with st.sidebar:
+        st.title("Update Vector Store:")
+        
+        if st.button("Vectors Update"):
+            with st.spinner("Processing..."):
+                docs = data_ingestion()
+                get_vector_store(docs)
+                st.success("Done")
+
+    if st.button("Claude Output"):
+        with st.spinner("Processing..."):
+            faiss_index = FAISS.load_local("faiss_index", bedrock_embeddings)
+            llm=get_claude_llm()
+            
+            #faiss_index = get_vector_store(docs)
+            st.write(get_response_llm(llm,faiss_index,user_question))
+            st.success("Done")
+
+    if st.button("Llama2 Output"):
+        with st.spinner("Processing..."):
+            faiss_index = FAISS.load_local("faiss_index", bedrock_embeddings)
+            llm=get_llama2_llm()
+            
+            #faiss_index = get_vector_store(docs)
+            st.write(get_response_llm(llm,faiss_index,user_question))
+            st.success("Done")
+    
+    if st.button("Titan Output"):
+        with st.spinner("Processing..."):
+            faiss_index = FAISS.load_local("faiss_index", bedrock_embeddings)
+            llm=get_titan_llm()
+            
+            #faiss_index = get_vector_store(docs)
+            st.write(get_response_llm(llm,faiss_index,user_question))
+            st.success("Done")
+
+    if st.button("Cohere Output"):
+        with st.spinner("Processing..."):
+            faiss_index = FAISS.load_local("faiss_index", bedrock_embeddings)
+            llm=get_cohere_llm()
+            
+            #faiss_index = get_vector_store(docs)
+            st.write(get_response_llm(llm,faiss_index,user_question))
+            st.success("Done")
+
+    if st.button("Jurassic Output"):
+        with st.spinner("Processing..."):
+            faiss_index = FAISS.load_local("faiss_index", bedrock_embeddings)
+            llm=get_jurassic_llm()
+            
+            #faiss_index = get_vector_store(docs)
+            st.write(get_response_llm(llm,faiss_index,user_question))
+            st.success("Done")
+
+if __name__ == "__main__":
+    main()
